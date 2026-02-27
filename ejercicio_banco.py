79b2d768bd709473c7c4a54bb276301b7d7ef06a
Luis Mejia <lmejiaidep@gmail.com>
2026-02-27 15:01:43 -0500

    Validaciones para la opcion retirar cuando el saldo es negativo

--- ejercicio_banco.py
+++ ejercicio_banco.py
@@ -1,6 +1,8 @@
 
 saldo_inicial=1000
 canti_operaciones=0
+#Variable bool que me va a permitir verificar los datos ingresados por el usuario
+validacion=True
 
 print("CAJERO AUTOMATICO")
 canti_operaciones=input("Ingrese la cantidad de operaciones que desea realizar: ")
@@ -12,12 +14,17 @@ try:
         print("2. Retirar")
         print("3. Consultar saldo")
         op= int(input("Seleccione una opción: "))
-        if op==2:
-            cantidad= int(input("Ingrese la cantidad a retirar: "))
-            if cantidad>saldo_inicial:
-                print("Tu saldo es insuficiente para realizar la transacción.")
-            else:
-                saldo_inicial= saldo_inicial-cantidad
-                print("Transacción exitosa. Su nuevo saldo es: ", saldo_inicial)
+        while validacion:
+            if op==2:
+                cantidad= int(input("Ingrese la cantidad a retirar: "))
+                if cantidad>saldo_inicial:
+                    print("Tu saldo es insuficiente para realizar la transacción.")
+                    validacion=False
+                elif cantidad <0:
+                    print("No puedes retirar una cantidad negativa.")
+                else:
+                    saldo_inicial= saldo_inicial-cantidad
+                    print("Transacción exitosa. Su nuevo saldo es: ", saldo_inicial)
+                    validacion=False
 except:
     print("Error: Debe ingresar un número entero para la cantidad de operaciones.")
