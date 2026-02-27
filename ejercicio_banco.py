f36f17985a5cab8e8e42aef91e93e36132ab1448
Luis Mejia <lmejiaidep@gmail.com>
2026-02-27 14:15:06 -0500

    Realizado operaciones básicas

--- ejercicio_banco.py
+++ ejercicio_banco.py
@@ -3,3 +3,12 @@ saldo_inicial=1000
 canti_operaciones=0
 
 print("CAJERO AUTOMATICO")
+canti_operaciones=input("Ingrese la cantidad de operaciones que desea realizar: ")
+try:
+    canti_operaciones=int(canti_operaciones)
+    for i in range(canti_operaciones):
+        print("1. Depositar")
+        print("2. Retirar")
+        print("3. Consultar saldo")
+except:
+    print("Error: Debe ingresar un número entero para la cantidad de operaciones.")
