2c485d517046a77addcd1f0561605860571c6719
Luis Mejia <lmejiaidep@gmail.com>
2026-02-27 14:33:56 -0500

    funcionalidad de retirar

--- ejercicio_banco.py
+++ ejercicio_banco.py
@@ -10,5 +10,13 @@ try:
         print("1. Depositar")
         print("2. Retirar")
         print("3. Consultar saldo")
+        op= int(input("Seleccione una opción: "))
+        if op==2:
+            cantidad= int(input("Ingrese la cantidad a retirar: "))
+            if cantidad>saldo_inicial:
+                print("Tu saldo es insuficiente para realizar la transacción.")
+            else:
+                saldo_inicial= saldo_inicial-cantidad
+                print("Transacción exitosa. Su nuevo saldo es: ", saldo_inicial)
 except:
     print("Error: Debe ingresar un número entero para la cantidad de operaciones.")
